bdfd4ed7833adeecd6fab518ff435e0ab1216c2f
Daniel Cassidy <mail@danielcassidy.me.uk>
2019-01-07 01:31:21 +0000

    Implement skeleton of tokenizer.

--- tokenizer.ts
+++ tokenizer.ts
@@ -2,3 +2,33 @@ export interface Token {
     readonly type: "word" | "operator";
     readonly text: string;
 }
+
+export function tokenize(text: string): ReadonlyArray<Token> {
+    let state: State = {
+        mode: "token-boundary",
+        tokens: [],
+        text,
+        position: 0
+    };
+
+    while (state.mode !== "end") {
+        state = step(state);
+    }
+
+    return state.tokens;
+}
+
+interface State {
+    readonly mode: "token-boundary" | "end";
+    readonly tokens: ReadonlyArray<Token>;
+    readonly text: string;
+    readonly position: number;
+}
+
+function step(state: State): State {
+    if (state.mode === "end") {
+        throw new Error("Step past end of character stream");
+    }
+
+    throw new Error("Not implemented");
+}
